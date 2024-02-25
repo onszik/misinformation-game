cd461e351199a4a3fa5ffe99ec28c272f09a6c14
onszik <popovviktor541@gmail.com>
2024-02-25 14:05:23 +0100

    dialogue tree working
    dodadov prviot level vo noviot dialogue tree sistem

---
+++ misinformation/Assets/Scripts/NodeBasedDialogueSystem-master/Editor/Graph/StoryGraph.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Subtegral.DialogueSystem.DataContainers;
+
+namespace Subtegral.DialogueSystem.Editor
+{
+    public class StoryGraph : EditorWindow
+    {
+        private string _fileName = "New Narrative";
+
+        private StoryGraphView _graphView;
+        private DialogueContainer _dialogueContainer;
+
+        [MenuItem("Graph/Narrative Graph")]
+        public static void CreateGraphViewWindow()
+        {
+            var window = GetWindow<StoryGraph>();
+            window.titleContent = new GUIContent("Narrative Graph");
+        }
+
+        private void ConstructGraphView()
+        {
+            _graphView = new StoryGraphView(this)
+            {
+                name = "Narrative Graph",
+            };
+            _graphView.StretchToParentSize();
+            rootVisualElement.Add(_graphView);
+        }
+
+        private void GenerateToolbar()
+        {
+            var toolbar = new Toolbar();
+
+            var fileNameTextField = new TextField("File Name:");
+            fileNameTextField.SetValueWithoutNotify(_fileName);
+            fileNameTextField.MarkDirtyRepaint();
+            fileNameTextField.RegisterValueChangedCallback(evt => _fileName = evt.newValue);
+            toolbar.Add(fileNameTextField);
+
+            toolbar.Add(new Button(() => RequestDataOperation(true)) {text = "Save Data"});
+
+            toolbar.Add(new Button(() => RequestDataOperation(false)) {text = "Load Data"});
+            // toolbar.Add(new Button(() => _graphView.CreateNewDialogueNode("Dialogue Node")) {text = "New Node",});
+            rootVisualElement.Add(toolbar);
+        }
+
+        private void RequestDataOperation(bool save)
+        {
+            if (!string.IsNullOrEmpty(_fileName))
+            {
+                var saveUtility = GraphSaveUtility.GetInstance(_graphView);
+                if (save)
+                    saveUtility.SaveGraph(_fileName);
+                else
+                    saveUtility.LoadNarrative(_fileName);
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("Invalid File name", "Please Enter a valid filename", "OK");
+            }
+        }
+
+        private void OnEnable()
+        {
+            ConstructGraphView();
+            GenerateToolbar();
+            GenerateMiniMap();
+            GenerateBlackBoard();
+        }
+
+        private void GenerateMiniMap()
+        {
+            var miniMap = new MiniMap {anchored = false};
+            Vector2 cords = new Vector2(this.position.width - 10, 30);
+            miniMap.SetPosition(new Rect(cords.x, cords.y, 200, 140));
+            _graphView.Add(miniMap);
+
+            var toggleButton = new Toggle("Show Minimap")
+            {
+                value = true
+            };
+            toggleButton.transform.position = new Vector2(0, 20);
+            _graphView.Add(toggleButton);
+
+            toggleButton.RegisterValueChangedCallback(evt =>
+            {
+                if (evt.newValue == true)
+                {
+                    _graphView.Add(miniMap);
+                }
+                else
+                {
+                    _graphView.Remove(miniMap);
+                }
+            });
+        }
+
+        private void GenerateBlackBoard()
+        {
+            var blackboard = new Blackboard(_graphView);
+            blackboard.Add(new BlackboardSection {title = "Exposed Variables"});
+            blackboard.addItemRequested = _blackboard =>
+            {
+                _graphView.AddPropertyToBlackBoard(ExposedProperty.CreateInstance(), false);
+            };
+            blackboard.editTextRequested = (_blackboard, element, newValue) =>
+            {
+                var oldPropertyName = ((BlackboardField) element).text;
+                if (_graphView.ExposedProperties.Any(x => x.PropertyName == newValue))
+                {
+                    EditorUtility.DisplayDialog("Error", "This property name already exists, please chose another one.",
+                        "OK");
+                    return;
+                }
+
+                var targetIndex = _graphView.ExposedProperties.FindIndex(x => x.PropertyName == oldPropertyName);
+                _graphView.ExposedProperties[targetIndex].PropertyName = newValue;
+                ((BlackboardField) element).text = newValue;
+            };
+            blackboard.SetPosition(new Rect(10,30,200,300));
+            _graphView.Blackboard = blackboard;
+
+            var toggleButton = new Toggle("Show Blackboard")
+            {
+                value = false
+            };
+            toggleButton.transform.position = new Vector2(0, 20);
+            _graphView.Add(toggleButton);
+
+            toggleButton.RegisterValueChangedCallback(evt =>
+            {
+                if (evt.newValue == true)
+                {
+                    _graphView.Add(blackboard);
+                }
+                else
+                {
+                    _graphView.Remove(blackboard);
+                }
+            });
+        }
+
+        private void OnDisable()
+        {
+            rootVisualElement.Remove(_graphView);
+        }
+    }
+}
---
+++ misinformation/Assets/Scripts/NodeBasedDialogueSystem-master/Editor/Nodes/DialogueNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace Subtegral.DialogueSystem.Editor
+{
+    public class DialogueNode : Node
+    {
+        public string DialogueText;
+        public string GUID;
+        public bool EntyPoint = false;
+    }
+
+    public class TweetNode : DialogueNode 
+    {
+        public int Value = 0;
+        public string Username = "@NewUser";
+    }
+}
